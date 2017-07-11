c16e505d41855f012272a9785fe12fd7bcf25d36
yanhappiness <457029997@qq.com>
2017-07-11 18:16:15 +0800

    file

---
+++ API/knob.js
@@ -0,0 +1,26 @@
+
+//旋转按钮脚本
+$(function(){
+
+	// 初始化脚本
+	var sigknob0 = {   //定于两个坐标值比较
+		x:0,
+		y:0,
+		tan:0
+	}
+	var sigknob1 = {
+		x:0,
+		y:0,
+		tan:0
+	}
+
+	// 鼠标离开任意一个子元素或所选元素触发
+	function sigMouseout(event){
+		var sigknob.x = event.pageX;
+		var sibknob.y = event.pageY;
+		var sigknob.tan = sibknob.y/sibknob.x;
+		if(sigknob.x*){
+			
+		}
+	}
+})
